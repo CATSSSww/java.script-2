dff84f544f498c3385ab63d6618d7356791ffd0f
CATSSSww <117073206+CATSSSww@users.noreply.github.com>
2023-01-18 18:08:12 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+'use strict';
+// Встроенные функции: console.log .replace .substr .substring .slice .toUpperCase .length
+// Переменные: let
+// Функции: function
+// Аргументы: 
+// Параметры:
+
+
+/*let txt = 'JS'
+console.log(txt.toLowerCase())*/
+
+/*let txt = 'я люблю JS!'
+console.log(txt.substr(2,5),txt.substr(8,2))
+console.log(txt.substring(2,7),txt.substring(8,10))
+console.log(txt.slice(2,7),txt.slice(8,10))*/
+
+/*let txt = 'я люблю JS!'
+console.log(txt.indexOf('люблю'))*/
+
+/*let txt = 'qwertyu werywewr eryertre utryrtey'
+let n = 10
+let a = txt.length
+if (n < a){
+    let result = (txt.substr(1,n))
+    console.log(result ,'...')
+}
+else{
+    let result = txt
+    console.log(result)
+}*/
+
+/*let txt = 'Я-люблю-JS!'
+console.log(txt.replace(/-/g, '!'))*/
+
+/*let txt = 'Я люблю JS!'
+let words = txt.split(' ')
+console.log(words[1])*/
+
+/*let txt = 'Я люблю JS!'
+let words = txt.split('')
+console.log(words[2])*/
+
